10eb41d71a62142a68555c00490919549ce9069b
Santanil Basak <santanilbasak.com>
2021-03-07 20:12:02 +0530

    First commit
    Uploading all files

---
+++ banking/src/test/java/com/banking/pageObjects/LoginPage.java
@@ -0,0 +1,40 @@
+package com.banking.pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+	
+	WebDriver ldriver;
+	
+	LoginPage(WebDriver rdriver){
+		ldriver = rdriver;
+		PageFactory.initElements(rdriver, this);
+	}
+
+	
+	@FindBy(name="uid")
+	WebElement txtUserName;
+	
+	@FindBy(name="password")
+	WebElement txtPassword;
+	
+	@FindBy(name="btnLogin")
+	WebElement btnLogin;
+	
+	
+	public void setUserName(String uname) {
+		txtUserName.sendKeys(uname);
+	}
+
+	
+	public void setPassword(String pwd) {
+		txtPassword.sendKeys(pwd);
+	}
+	
+	public void clickLogin() {
+		btnLogin.click();
+	}
+}
